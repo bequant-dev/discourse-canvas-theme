e215c53d8552d63f377feb8f629abe85560d3f3f
Rajat Jain <1997.rajatjain@gmail.com>
2025-08-26 11:15:33 +0530

    Added bequant redirect

--- javascripts/discourse/initializers/logo-redirect.js
+++ javascripts/discourse/initializers/logo-redirect.js
@@ -4,15 +4,8 @@ export default {
   name: "logo-redirect",
   initialize() {
     withPluginApi("0.8.31", api => {
-      // Simple logo redirect to bequant.dev
-      const logoLink = document.querySelector('.home-logo-wrapper-outlet .title a[href="/"]');
-      if (logoLink && !logoLink.hasAttribute('data-redirect-added')) {
-        logoLink.setAttribute('data-redirect-added', 'true');
-        logoLink.addEventListener('click', (e) => {
-          e.preventDefault();
-          window.location.href = 'https://bequant.dev';
-        });
-      }
+      // Use the recommended Discourse API to change logo href
+      api.changeWidgetSetting('home-logo', 'href', 'https://bequant.dev');
     });
   }
 }; 
